4be1a8d62bbad98ae7b21ea2aafcfdc3a8c1c0ca
peterloi96 <peterloiacono2014@gmail.com>
2017-04-22 22:52:10 -0400

    Finished adding class names of different FSM nodes and arcs in header

--- FSM.h
+++ FSM.h
@@ -7,51 +7,65 @@
 
 class FSM
 {
-public:
-	FSM(int, int, std::string);
-	void fillStates();
-	void addNode(std::string);
-	
-private:
-	int states;
-	int inBits;
-	int maxArcs;
-	std::string typeFSM;
-	std::vector<Node*> nodes;
-	std::vector<Arc*> arcs;
-}
+	public:
+		FSM(int, int, std::string);
+		void fillStates();
+		void addNode(std::string);
+
+	private:
+		int states;
+		int inBits;
+		int maxArcs;
+		std::string typeFSM;
+		std::vector<Node*> nodes;
+		std::vector<Arc*> arcs;
+};
  
 class Node
 {
-public:
-	Node(std::string);
-private:
-	std::string name;
-}
+	public:
+		Node(std::string);
+	private:
+		std::string name;
+};
 
 class Arc
 {
-public:
-	Arc(Node*, Node*, std::string);
-private:
-	std::string node1;
-	std::string node2;
-	Node * pair[2];
-	std::string inputs;
-}
+	public:
+		Arc(Node*, Node*, std::string);
+	private:
+		std::string node1;
+		std::string node2;
+		Node * pair[2];
+		std::string inputs;
+};
 
 class MooreNode : public Node
 {
-public:
-	MooreNode(std::string, std::string);
-private:
-	std::string output;
-}
+	public:
+		MooreNode(std::string, std::string);
+	private:
+		std::string output;
+};
+
+class MooreArc : public Arc
+{
+	public:
+		MooreArc()
+	private:
+
+};
+
+class MealyNode : public Node
+{
+
+
+};
 
 class MealyArc : public Arc
 {
-public:
-	MealyArc(Node*, Node*, std::string, std::string);
-private:
-	std::string output;
-}
+	public:
+		MealyArc(Node*, Node*, std::string, std::string);
+	private:
+		std::string output;
+};
