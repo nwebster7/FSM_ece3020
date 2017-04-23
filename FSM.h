7f2640190e06b160465bf2a98630359d45996c85
peterloi96 <peterloiacono2014@gmail.com>
2017-04-23 14:59:35 -0400

    Fixed header

--- FSM.h
+++ FSM.h
@@ -5,27 +5,6 @@
 //program will ask user to define # of states, # of input bits, and Mealy/Moore
 //before requesting further input
 
-<<<<<<< HEAD
-class FSM
-{
-	public:
-		FSM(int , int , std::string );
-		~FSM();
-		void fillStates();
-		void addNode(std::string );
-		void fillArcs();
-
-	private:
-		int states;
-		int inBits;
-		int maxArcs;
-		std::string typeFSM;
-		std::vector<Node*> nodes;
-		std::vector<Arc*> arcs;
-};
- 
-=======
->>>>>>> 5a7ea07af108333cc6829b0808290229cbec38a0
 class Node
 {
 	public:
